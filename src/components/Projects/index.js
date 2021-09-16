5bb3ba6cb40f484204e4f333dde2755e24ae50b0
Scuobahia <antonio.sr8808@gmail.comgit config --global user.name Antonio>
2021-09-15 21:18:30 -0600

    2.0

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -1,12 +1,39 @@
-import React from 'react'
 
-function Projects() {
+import React from 'react';
 
-  return (
-    <section id="projects">
-   
-    </section>
-  )
-}
+import { BlogCard, CardInfo, ExternalLinks, GridContainer, HeaderThree, Hr, Tag, TagList, TitleContent, UtilityList, Img } from './ProjectsStyles';
+import { Section, SectionTitle } from '../../Styles/PageComponents';
+import { projects } from '../../constants/projectConstants';
+
+const Projects = () => (
+  <Section nopadding id="projects">
+    <SectionTitle main>Projects</SectionTitle>
+    <GridContainer>
+      {projects.map((p, i) => {
+        return (
+          <BlogCard key={i}>
+          <Img src={p.image} />
+            <TitleContent>
+              <HeaderThree title>{p.title}</HeaderThree>
+              <Hr />
+            </TitleContent>
+            <CardInfo className="card-info">{p.description}</CardInfo>
+            <div>
+              <TagList>
+                {p.tags.map((t, i) => {
+                  return <Tag key={i}>{t}</Tag>;
+                })}
+              </TagList>
+            </div>
+            <UtilityList>
+              <ExternalLinks href={p.code}>Code</ExternalLinks>
+              <ExternalLinks href={p.source}>Source</ExternalLinks>
+            </UtilityList>
+          </BlogCard>
+        );
+      })}
+    </GridContainer>
+  </Section>
+);
 
 export default Projects;
