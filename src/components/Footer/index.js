ee6bcdf4912351e4e607ae7ae1d254bb06d69922
Scuobahia <antonio.sr8808@gmail.comgit config --global user.name Antonio>
2021-09-16 11:54:42 -0600

    Footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,12 +1,45 @@
-import React from 'react'
+import React from 'react';
+import { AiFillGithub, AiFillInstagram, AiFillLinkedin, AiFillFacebook } from 'react-icons/ai';
 
-function Footer() {
+import { SocialIcons } from '../Header/HeaderStyles';
+import { CompanyContainer, FooterWrapper, LinkColumn, LinkItem, LinkList, LinkTitle, Slogan, SocialContainer, SocialIconsContainer } from './FooterStyles';
 
+const Footer = () => {
   return (
-   <section id="footer">
-
-   </section>
-  )
-}
+    <FooterWrapper>
+      <LinkList>
+        <LinkColumn>
+          <LinkTitle>Call</LinkTitle>
+          <LinkItem href="tel:801-822-2296">801-822-2296</LinkItem>
+        </LinkColumn>
+        <LinkColumn>
+          <LinkTitle>Email</LinkTitle>
+          <LinkItem href="mailto:antonio.sr8808@gmail.com">
+            antonio.sr8808@gmail.com
+          </LinkItem>
+        </LinkColumn>
+      </LinkList>
+      <SocialIconsContainer>
+        <CompanyContainer>
+          <Slogan>Creativity Unleashed</Slogan>
+        </CompanyContainer>
+        <SocialContainer>
+          <SocialIcons href="https://github.com/Scuobahia">
+            <AiFillGithub size="3rem" />
+          </SocialIcons>
+          <SocialIcons href="https://linkedin.com">
+            <AiFillLinkedin size="3rem" />
+          </SocialIcons>
+          <SocialIcons href="https://instagram.com">
+            <AiFillInstagram size="3rem" />
+          </SocialIcons>
+          <SocialIcons href="https://facebook.com">
+          <AiFillFacebook size="3rem"/>
+        </SocialIcons>
+        </SocialContainer>
+      </SocialIconsContainer>
+    </FooterWrapper>
+  );
+};
 
 export default Footer;
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -24,7 +24,7 @@ const Header = () =>  (
       </li>        
     </Div2>
       <Div3>
-        <SocialIcons href="https://github.com">
+        <SocialIcons href="https://github/Scuobahia.com">
           <AiFillGithub size="3rem" />
         </SocialIcons>
         <SocialIcons href="https://linkedin.com">
