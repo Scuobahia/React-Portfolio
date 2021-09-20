161053e0f04dbdab8e3fc2819ef6a8d233745477
Scuobahia <antonio.sr8808@gmail.comgit config --global user.name Antonio>
2021-09-19 21:06:57 -0600

    selfie image added

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Fade from 'react-reveal/Fade';
 import { Section, SectionText, SectionTitle } from '../../Styles/PageComponents';
+import ME from '../../me.jpeg';
 const Hero = () => (
   <>
     <Section>
@@ -9,6 +10,7 @@ const Hero = () => (
           Welcome <br/> To <br />
           My Portfolio
         </SectionTitle>
+        <img className="ME" src ={ME}/>
         <Fade right big>
         <SectionText>
         Taking the web to a whole new level!
